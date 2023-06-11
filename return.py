63093547f7816fde94c286fd4d46122be98ab7a4
kme-developer <130229450+kme-developer@users.noreply.github.com>
2023-06-11 09:49:38 +0900

    Update return.py

--- return.py
+++ return.py
@@ -1,8 +1,33 @@
 x = 1
 
-def test(y):
-  global x
+def test_1(y):
+  global x # <= important!
+  print(x)
   x = y
   return x
 
-print(test(2)) # 2
+print(x) # 1
+print(test_1(2)) # 1 2
+print(x) # 2
+
+''''''
+
+def test_2():
+  print(x)
+  x = x + 1
+  return x
+
+test_2() # cannot access local variable 'x' where it is not associated with a value ('x' is undefined)
+
+''''''
+
+def test_3():
+  global x # <= important!
+  print(x)
+  x = x + 1
+  return x
+
+print(x) # 1
+test_3() # 1
+print(test_3()) # 2
+print(x) # 3
